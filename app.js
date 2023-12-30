8c215d8e8888d9cc90a76ee4e5ea3d3c9807cdff
pbaki <pbaki999@gmail.com>
2023-12-30 21:46:10 +0100

    mongodb connect + little cleanup

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var blogRouter = require("./routes/blog");
+var usersRouter = require("./routes/users");
+var commentsRouter = require("./routes/comments");
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log("Connencted to DB");
+}
+
+app.use("/", blogRouter);
+app.use("/", usersRouter);
+app.use("/", commentsRouter);
 
 module.exports = app;
