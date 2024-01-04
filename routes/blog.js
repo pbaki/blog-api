f2e01bcf3aea2191cabe804b17a19ab04401719c
pbaki <pbaki999@gmail.com>
2024-01-04 19:23:19 +0100

    name based search

--- routes/blog.js
+++ routes/blog.js
@@ -5,23 +5,26 @@ const blogController = require("../controller/blogController");
 router.get("/posts", blogController.get_blogposts);
 router.post("/posts", blogController.create_blogpost);
 
-router.get("/posts/:postid", blogController.get_single_blogpost);
-router.put("/posts/:postid", blogController.update_single_blogpost);
-router.delete("/posts/:postid", blogController.delete_single_blogpost);
+router.get("/posts/:posttitle", blogController.get_single_blogpost);
+router.put("/posts/:posttitle", blogController.update_single_blogpost);
+router.delete("/posts/:posttitle", blogController.delete_single_blogpost);
 
-router.get("/posts/:postid/comments", blogController.get_blogpost_comments);
-router.post("/posts/:postid/comments", blogController.create_blogpost_comment);
+router.get("/posts/:posttitle/comments", blogController.get_blogpost_comments);
+router.post(
+  "/posts/:posttitle/comments",
+  blogController.create_blogpost_comment
+);
 
 router.get(
-  "/posts/:postid/comments/:commentid",
+  "/posts/:posttitle/comments/:commentid",
   blogController.get_single_blogpost_comment
 );
 router.put(
-  "/posts/:postid/comments/:commentid",
+  "/posts/:posttitle/comments/:commentid",
   blogController.update_single_blogpost_comment
 );
 router.delete(
-  "/posts/:postid/comments/:commentid",
+  "/posts/:posttitle/comments/:commentid",
   blogController.delete_single_blogpost_comment
 );
 
--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,19 @@ const userController = require("../controller/userController");
 router.get("/users", userController.get_users);
 router.post("/users", userController.create_user);
 
-router.get("/users/:userid", userController.get_single_user);
-router.put("/users/:userid", userController.update_single_user);
-router.delete("/users/:userid", userController.delete_single_user);
+router.get("/users/:username", userController.get_single_user);
+router.put("/users/:username", userController.update_single_user);
+router.delete("/users/:username", userController.delete_single_user);
 
-router.get("/users/:userid/posts", userController.get_user_posts);
-router.get("/users/:userid/posts/:postid", userController.get_single_user_post);
-
-router.get("/users/:userid/comments", userController.get_user_comments);
+router.get("/users/:username/posts", userController.get_user_posts);
 router.get(
-  "/users/:userid/comments/:commentid",
+  "/users/:username/posts/:posttitle",
+  userController.get_single_user_post
+);
+
+router.get("/users/:username/comments", userController.get_user_comments);
+router.get(
+  "/users/:username/comments/:commentid",
   userController.get_single_user_comment
 );
 
